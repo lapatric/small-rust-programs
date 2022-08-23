88c310c27f1db241bdedc53b14df9ac4db0ad5cd
lapatric <42653152+lapatric@users.noreply.github.com>
2022-08-23 15:20:08 +0200

    initial commit

---
+++ hello_cargo/target/debug/deps/hello_cargo-be7d286e7ce7f2e9.d
@@ -0,0 +1,5 @@
+/Users/patrick/Documents/Rust/small-rust-programs/hello_cargo/target/debug/deps/hello_cargo-be7d286e7ce7f2e9: src/main.rs
+
+/Users/patrick/Documents/Rust/small-rust-programs/hello_cargo/target/debug/deps/hello_cargo-be7d286e7ce7f2e9.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/patrick/Documents/Rust/small-rust-programs/hello_cargo/target/debug/hello_cargo: /Users/patrick/Documents/Rust/small-rust-programs/hello_cargo/src/main.rs
